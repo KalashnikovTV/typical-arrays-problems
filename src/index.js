bd06c9e195d061fedf182ce14bdef35f4dfc1b6a
KalashnikovTV <max.malaschkov@mail.ru>
2021-01-26 23:54:51 +0300

    complete min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
-}
+  let numMin = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < numMin) {
+      numMin = array[i];
+    }
+  }
 
-exports.max = function max (array) {
-  return 0;
-}
+  return numMin;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  let numMax = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] > numMax){
+      numMax = array[i];
+    }
+  }
+
+  return numMax;
+};
+
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+
+  let sum = array.reduce((a, b) => a + b);
+  return sum / array.length;
+};
